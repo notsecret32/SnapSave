d2fa51e0a62673c723bec971ec5965c210849707
Alexander Skvorchevskiy <sashaskvorhevskiy@gmail.com>
2024-11-01 17:20:14 +0300

    fix: fixed errors in storybook operation

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,10 +1,8 @@
 import type { StorybookConfig } from '@storybook/react-vite'
+import path from 'path'
 
 const config: StorybookConfig = {
-  stories: [
-    '../src/renderer/src/**/*.mdx',
-    '../src/renderer/src/**/*.stories.@(js|jsx|mjs|ts|tsx)'
-  ],
+  stories: ['../src/renderer/src/components/**/*.stories.@(ts|tsx)'],
   addons: [
     '@storybook/addon-onboarding',
     '@storybook/addon-essentials',
@@ -14,6 +12,15 @@ const config: StorybookConfig = {
   framework: {
     name: '@storybook/react-vite',
     options: {}
+  },
+  viteFinal: async (config) => {
+    if (config.resolve?.alias) {
+      config.resolve.alias = {
+        ...config.resolve?.alias,
+        '@': path.resolve(__dirname, '../src/renderer/src/')
+      }
+    }
+    return config
   }
 }
 export default config
