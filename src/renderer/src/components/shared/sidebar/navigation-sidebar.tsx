21c78b75255fe3e26ef6bf501d2d726abe9b4580
Alexander Skvorchevskiy <43242733+notsecret32@users.noreply.github.com>
2024-11-06 22:57:23 +0300

    refactor: it was decided to redesign the ui

--- src/renderer/src/components/shared/sidebar/navigation-sidebar.tsx
+++
@@ -1,8 +0,0 @@
-import { cn } from '@/utils'
-import { ComponentProps, FC } from 'react'
-
-type NavigationSidebarProps = Omit<ComponentProps<'aside'>, 'children'>
-
-export const NavigationSidebar: FC<NavigationSidebarProps> = ({ className, ...rest }) => {
-  return <aside className={cn('flex w-20 h-screen bg-blue-600', className)} {...rest}></aside>
-}
--- src/renderer/src/pages/Dashboard.tsx
+++ src/renderer/src/pages/Dashboard.tsx
@@ -1,16 +1,5 @@
-import { Content } from '@/components/shared'
-import { RootLayout } from '@/components/shared/layout'
-import { DashboardSidebar, NavigationSidebar } from '@/components/shared/sidebar'
 import { FC } from 'react'
 
 export const Dashboard: FC = () => {
-  return (
-    <RootLayout>
-      <NavigationSidebar />
-      <DashboardSidebar />
-      <Content>
-        <h1>Dashboard</h1>
-      </Content>
-    </RootLayout>
-  )
+  return <div>Dashboard</div>
 }
