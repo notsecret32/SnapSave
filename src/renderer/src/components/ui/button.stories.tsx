6c8f85891feb5fc35502d4952139e01c150ed92f
Alexander Skvorchevskiy <43242733+notsecret32@users.noreply.github.com>
2024-11-07 00:23:47 +0300

    feat: added new options for the button

--- src/renderer/src/components/ui/button.stories.tsx
+++ src/renderer/src/components/ui/button.stories.tsx
@@ -2,16 +2,33 @@ import type { Meta, StoryObj } from '@storybook/react'
 import { Button } from './button'
 
 const meta: Meta<typeof Button> = {
-  title: 'Components/Button',
+  title: 'UI/Button',
   component: Button
 }
 
 export default meta
 type Story = StoryObj<typeof Button>
 
-export const Primary: Story = {
+export const DefaultStory: Story = {
+  name: 'Default',
   args: {
     children: 'Button',
     variant: 'default'
+  }
+}
+
+export const SecondaryStory: Story = {
+  name: 'Secondary',
+  args: {
+    children: 'Button',
+    variant: 'secondary'
+  }
+}
+
+export const OutlinedStory: Story = {
+  name: 'Outlined',
+  args: {
+    children: 'Button',
+    variant: 'outlined'
   }
 }
