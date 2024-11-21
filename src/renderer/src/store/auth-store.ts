83f815fec2f085522f91724dc2268c8c7097db01
Alexander Skvorchevskiy <43242733+notsecret32@users.noreply.github.com>
2024-11-21 19:21:29 +0300

    fix: fixed a behavior where data was reset after a page reload

--- src/renderer/src/store/auth-store.ts
+++ src/renderer/src/store/auth-store.ts
@@ -1,4 +1,4 @@
-import { supabase } from '@/config/supabase'
+import { supabase } from '@/config'
 import { User } from '@/types'
 import {
   AuthError,
@@ -8,6 +8,7 @@ import {
   SignUpWithPasswordCredentials
 } from '@supabase/supabase-js'
 import { create } from 'zustand'
+import { devtools, persist } from 'zustand/middleware'
 
 export interface AuthStore {
   user: User | null
@@ -18,58 +19,66 @@ export interface AuthStore {
   signOut: (options?: SignOut) => Promise<void>
 }
 
-export const useAuthStore = create<AuthStore>((set) => ({
-  user: null,
-  session: null,
-  error: null,
-  signIn: async (credentials) => {
-    const {
-      data: { user, session },
-      error
-    } = await supabase.auth.signInWithPassword(credentials)
+export const useAuthStore = create<AuthStore>()(
+  devtools(
+    persist(
+      (set) => ({
+        user: null,
+        session: null,
+        error: null,
+        signIn: async (credentials) => {
+          const {
+            data: { user, session },
+            error
+          } = await supabase.auth.signInWithPassword(credentials)
 
-    if (error) {
-      set({ error })
-      return
-    }
+          if (error) {
+            set({ error })
+            return
+          }
 
-    if (!user || !session) {
-      set({ error: new AuthError('Something went wrong') })
-      return
-    }
+          if (!user || !session) {
+            set({ error: new AuthError('Something went wrong') })
+            return
+          }
 
-    set({ user, session })
-  },
-  signUp: async (credentials) => {
-    const {
-      data: { user, session },
-      error
-    } = await supabase.auth.signUp(credentials)
+          set({ user, session })
+        },
+        signUp: async (credentials) => {
+          const {
+            data: { user, session },
+            error
+          } = await supabase.auth.signUp(credentials)
 
-    if (error) {
-      set({ error })
-      return
-    }
+          if (error) {
+            set({ error })
+            return
+          }
 
-    if (!user || !session) {
-      set({ error: new AuthError('Something went wrong') })
-      return
-    }
+          if (!user || !session) {
+            set({ error: new AuthError('Something went wrong') })
+            return
+          }
 
-    set({ user, session })
-  },
-  signOut: async (options) => {
-    const { error } = await supabase.auth.signOut(options)
+          set({ user, session })
+        },
+        signOut: async (options) => {
+          const { error } = await supabase.auth.signOut(options)
 
-    if (error) {
-      set({ error })
-      return
-    }
+          if (error) {
+            set({ error })
+            return
+          }
 
-    set({
-      user: null,
-      session: null,
-      error: null
-    })
-  }
-}))
+          set({
+            user: null,
+            session: null,
+            error: null
+          })
+        }
+      }),
+      { name: 'auth-storage' }
+    ),
+    { enabled: false }
+  )
+)
