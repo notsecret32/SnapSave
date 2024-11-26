f54177927fa5e05e3cde4a993b6be107c8b25660
Alexander Skvorchevskiy <43242733+notsecret32@users.noreply.github.com>
2024-11-27 01:30:27 +0300

    feat: added an `AppBar` without working buttons

--- src/renderer/src/components/shared/common/app-bar.tsx
+++ src/renderer/src/components/shared/common/app-bar.tsx
@@ -1,5 +1,32 @@
+import { Minimize, Minus, SunMoon, X } from 'lucide-react'
 import { FC } from 'react'
 
+const actions = [
+  {
+    id: 'minimize',
+    Icon: Minus
+  },
+  {
+    id: 'something',
+    Icon: Minimize
+  },
+  {
+    id: 'close',
+    Icon: X
+  }
+]
+
 export const AppBar: FC = () => {
-  return <div className="app-bar absolute inset-0 h-4 bg-transparent" />
+  return (
+    <div className="app-bar absolute inset-0 z-50 flex h-min items-center justify-end bg-transparent">
+      <div className="mr-1 rounded-md p-1 hover:bg-accent/90">
+        <SunMoon size={18} />
+      </div>
+      {actions.map(({ id, Icon }) => (
+        <div key={id} className="px-4 py-2 hover:bg-accent/90">
+          <Icon size={18} />
+        </div>
+      ))}
+    </div>
+  )
 }
